5cae8ad1a934ebbf67150ab13d43541c6117d49e
Shafia Ansar <shafiaansar142@gmail.com>
2022-09-02 05:56:37 -0800

    ''

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/home/Home'
 function App() {
   return (
     <div className="app">
-       App
+       <BrowserRouter>
+       <Routes>
+        <Route path='/' element={Home}/>
+       </Routes>
+       
+       </BrowserRouter>
     </div>
   );
 }
