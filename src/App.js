a0e9d089be5eec029374fd5e8224a405d9b2a8b0
Shafia Ansar <shafiaansar142@gmail.com>
2022-09-02 06:12:12 -0800

    'Navbar'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Home from './pages/home/Home'
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div className="app">
        <BrowserRouter>
+       <Navbar/>
        <Routes>
         <Route path='/' element={Home}/>
        </Routes>
