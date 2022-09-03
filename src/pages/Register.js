acdd52299bb9d90ae403e5348e805d57f23caed6
Shafia Ansar <shafiaansar142@gmail.com>
2022-09-03 09:15:50 -0800

    ''

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,24 +1,52 @@
-import React from 'react'
-
+import { useState } from "react"
+import React  from 'react'
+import {createUserWithEmailAndPassword} from "firebase/auth"
+import {auth} from  "../firebase"
 const Register = () => {
+    const [data ,setData] = useState({
+        name: "",
+        email: "",
+        password:"",
+        error: null,
+        loading: false
+
+    })
+    const { name,email,password,error,loading} = data
+    const handleChange = e =>{
+        setData({...data,[e.target.name]:e.target.value})
+    }
+    const handleSubmit= async e =>{
+        e.preventDefault()
+        if(!name || !email || !password){
+            setData({...data, error:'All fields are required'})
+        }
+        try {
+            const result = await createUserWithEmailAndPassword(auth,email,password)
+            console.log(result.user)
+        } catch (err) {
+            
+        }
+
+    }
   return (
     <section>
         <h3>
             Create An Account
         </h3>
-        <form className='form'>
+        <form className='form' onSubmit={handleSubmit}>
             <div className='input_container'>
                 <label htmlFor='name'>Name</label>
-                <input type='text' name ='name'/>
+                <input type='text' name ='name' value={name} onChange={handleChange}/>
             </div>
             <div className='input_container'>
-                <label htmlFor='email'>Email</label>
-                <input type='email' name ='email'/>
-            </div><div className='input_container'>
-                <label htmlFor='password'>PassWord</label>
-                <input type='password' name ='password'/>
+                <label htmlFor='email' value={name} onChange={handleChange}>Email</label>
+                <input type='email' name ='email' value={email} onChange={handleChange}/>
             </div>
-            
+            <div className='input_container'>
+                <label htmlFor='password'>PassWord</label>
+                <input type='password' name ='password' value={password} onChange={handleChange}/>
+            </div>
+            {error ? <p className='error'>{error}</p>:null}
             <div className='btn_container'>
                 <button className='btn'>
                 Register
