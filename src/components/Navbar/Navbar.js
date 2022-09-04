b80fc915189d0ef83ec3bd3c95f8cdae32e78d2d
Shafia Ansar <shafiaansar142@gmail.com>
2022-09-04 04:44:39 -0800

    'Logout'

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {auth,db} from '../../firebase'
+import {signOut} from 'firebase/auth'
+import {updateDoc,doc} from 'firebase/firestore'
 const Navbar = () => {
-  return (
+  const handleSignOut= async()=>{
+    await updateDoc(doc(db,'users', auth.currentUser.uid),{
+      isOnlineTrue:false
+    })
+    await signOut(auth)
+}
+return (
     <nav>
       <h3>
         <Link to='/'>Messenger</Link>
       </h3>
       <div>
-        
+        {auth.currentUser?(
+        <>
+        <Link to='/profile'>Profile</Link>
+        <button className='btn' onClick={handleSignOut}>Logout</button>
+        </>):(<>
         <Link to ='/register'>Register</Link>
-        <Link to='/login' >Login</Link>
+        <Link to='/login' >Login</Link></>)
+        } 
       </div>
 
 
