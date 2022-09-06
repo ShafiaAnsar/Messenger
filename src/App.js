81cbe6388759924a587b36abbdfb1c6a1f63c362
Shafia Ansar <shafiaansar142@gmail.com>
2022-09-06 00:27:09 -0800

    'Profile'

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import Navbar from './components/Navbar';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import AuthProvider from './context/auth';
-// import { AuthContext } from './context/auth'
 // import PrivateRoute from './components/PrivateRoute';
-// import { useContext } from 'react';
+import Profile from './pages/Profile';
 function App() {
     return (
   <AuthProvider>
@@ -16,6 +15,7 @@ function App() {
           <Routes>
           <Route path='/login' element={<Login/>}/>
           <Route path='/register' element={<Register/>}/>
+          <Route path='/profile' element={<Profile/>}/>
           <Route exact path='/' element={<Home/>}/>
         </Routes>
     </BrowserRouter>
---
+++ src/pages/Profile.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Camera from '../components/svg/Camera'
+const Profile = () => {
+  return (
+    <div className='profile_container'>
+      <div className='img_container'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyCbJoUCRscGfzySEtqoR5HtHnEOE0ux4r-A&usqp=CAU' alt='img' />
+        <div className='overlay'>
+            <div>
+                <label htmlFor='photo'>
+                    <Camera/>
+                </label>
+                <input type='file' accept='image/*' style={{display:'none'}} id='photo'/>
+            </div>
+        </div>
+      </div>
+      <div className='text_container'>
+        <h3>User Name</h3>
+        <p> User Email</p>
+        <hr/>
+        <small>Joined on : ...</small>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
