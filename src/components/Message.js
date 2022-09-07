2ae81e24de1acb36d3606c1fdbf65d38e6e48597
Shafia Ansar <shafiaansar142@gmail.com>
2022-09-07 06:31:16 -0800

    Styling

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React,{useRef,useEffect} from 'react'
 import Moment from 'react-moment'
-const Message = ({msg}) => {
+const Message = ({msg,user1}) => {
+    const scrollRef = useRef()
+    useEffect(()=>{
+        scrollRef.current?.scrollIntoView({behavior:'smooth'})
+    },[msg])
   return (
-    <div className='message_wrapper'>
-      <p>{
+    <div className={`message_wrapper ${msg.from === user1 ?'own':''}`} ref={scrollRef}
+    >
+      <p  className={msg.from ===user1 ?'me' :'friend'}>{
         msg.media?<img src={msg.media} alt={msg.text}/>:null
         }
         {msg.text}
