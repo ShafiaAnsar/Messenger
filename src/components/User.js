cb76487d087e87de6c81f813c921a75db688168e
Shafia Ansar <shafiaansar142@gmail.com>
2022-09-08 07:55:00 -0800

    modified

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Img from "../asset/img.jpg";
-
-const User = ({user,selectUser}) => {
-  return (
-    <div className='user_wrapper'onClick={() => selectUser(user)}>
+import {onSnapshot,doc} from 'firebase/firestore'
+import {db} from '../firebase'
+const User = ({user,user1,selectUser,chat}) => {
+  const user2 = user?.uid
+  const [data,setData] = useState('')
+  useEffect(()=>{
+        const id = user1 > user2 ? `${user1 + user2}` : `${user2 + user1}`;
+        let unsub = onSnapshot(doc(db, "lastMsg", id),doc =>{   
+          setData(doc.data())
+          }
+        )
+        return()=>unsub()
+  },[])
+    return (
+    <div className={`user_wrapper ${chat.name === user.name && 'selected_user'}`}onClick={() => selectUser(user)}>
         <div className='user_info'>
             <div className='user_detail'>
                 <img src={user.avatar || Img} alt='avatar' className='avatar'/>
@@ -13,7 +24,11 @@ const User = ({user,selectUser}) => {
             className={`user_status ${user.isOnLine ? "online" : "offline"}`}
           ></div>
         </div>
-      
+      {data &&(
+        <p className='truncate'>
+         <strong>{data.from === user1 ?"Me:":null}</strong>
+          {data.text}</p>
+      )}
     </div> 
   )
 }
